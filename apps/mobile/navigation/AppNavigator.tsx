b061458157b39ae23d3b91c4a0c0cdfb86a73697
Samuel <exterminationzls@gmail.com>
2026-03-17 19:09:32 -0400

    fix: stabilize EAS build - remove react-native-worklets, repo/tsconfig, react-dom, fix double NavigationContainer

--- apps/mobile/navigation/AppNavigator.tsx
+++ apps/mobile/navigation/AppNavigator.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from '../screens/LoginScreen';
 import JobListScreen from '../screens/JobListScreen';
 import JobDetailsScreen from '../screens/JobDetailsScreen';
